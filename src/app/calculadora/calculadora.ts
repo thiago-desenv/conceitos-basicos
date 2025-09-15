ee29b36024fd991aca002b76952b70f4dbc9d380
thiago_osim <thiago_osim@hotmail.com>
2025-09-15 20:09:05 -0300

    Criado lógica para realizar a soma

--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculadora.html',
   styleUrl: './calculadora.scss'
 })
 export class Calculadora {
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
 
+  calcularResultado() {
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
