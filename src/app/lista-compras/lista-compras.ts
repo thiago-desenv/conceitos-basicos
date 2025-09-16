2bce559eaf3ad0ff13386ebe06114fe0fd36b6f4
thiago_osim <thiago_osim@hotmail.com>
2025-09-16 20:25:07 -0300

    Criado lógica para captura do campo item ao submeter o form

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.html',
   styleUrl: './lista-compras.scss'
 })
 export class ListaCompras {
+  item: string = '';
 
+  adicionarItem() {
+    console.log('valor item: ', this.item);
+    this.item = '';
+  }
 }
