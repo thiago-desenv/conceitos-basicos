80d726bb4e71350fbcd8f2806fc95e91ba369375
thiago_osim <thiago_osim@hotmail.com>
2025-09-16 19:28:33 -0300

    Criado o componente lista-compras

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Calculadora } from './calculadora/calculadora';
+import { ListaCompras } from './lista-compras/lista-compras';
 
 @Component({
   selector: 'app-root',
-  imports: [Calculadora],
+  imports: [ListaCompras],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
