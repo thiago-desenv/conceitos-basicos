742217b6115817f14632e089e7157e5e764428ef
thiago_osim <thiago_osim@hotmail.com>
2025-09-21 19:46:55 -0300

    Criado método de riscar item comprado e limpar itens

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -15,11 +15,23 @@ export class ListaCompras {
   listaItem: ItemLista[] = [];
 
   adicionarItem() {
+    if(this.item === '') {
+      return;
+    }
+
     let itemLista = new ItemLista();
     itemLista.id = this.listaItem.length + 1;
     itemLista.nome = this.item;
     itemLista.comprado = this.comprado;
     this.listaItem.push(itemLista);
     this.item = '';
+  }
+
+  limparItens() {
+    this.listaItem = [];
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
   }
 }
